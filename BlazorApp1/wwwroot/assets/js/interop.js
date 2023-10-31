655470949318a6185ea1a6c50da4a025f5b9d04a
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-10-31 10:23:33 +0300

    DatePicker onchange eklendi.

--- BlazorApp1/wwwroot/assets/js/interop.js
+++ BlazorApp1/wwwroot/assets/js/interop.js
@@ -5,3 +5,25 @@
 function createAlert() {
     alert("Hey this is a alert!");
 }
+
+async function updateSelectedDate(selectedDate) {
+    DotNet.invokeMethodAsync($("MSBuildProjectName"), "UpdateSelectedDate", selectedDate)
+        .then(() => {
+            console.log("Date updated successfully in Blazor component.");
+        })
+        .catch(error => {
+            console.error("Error updating date in Blazor component: " + error);
+        });
+}
+
+function initializeDatePicker() {
+    const datePicker = $("#kt_datepicker_1").flatpickr({
+        onChange: function (selectedDates, dateStr, instance) {
+            if (selectedDates.length > 0) {
+                //const selectedDate = selectedDates[0].toString();
+                return "mumtaz";//selectedDates[0].toString();
+                //updateSelectedDate(selectedDate);
+            }
+        }
+    });
+}
